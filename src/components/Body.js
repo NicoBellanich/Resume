d45205bd90d97f1bdd0303e6882f4dea0b36216f
Nico Bellanich <nicolasbellanich@gmail.com>
2021-02-26 00:13:27 -0300

    Last changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,17 @@
 import React from "react";
 import classes from "./Body.module.css";
-import { HiMail as EmailIcon } from "react-icons/hi";
+import { HiMail as EmailIcon, HiChat as ChatIcon } from "react-icons/hi";
 import { AiFillLinkedin as LinkedinIcon } from "react-icons/ai";
-import { FaPhone as PhoneIcon } from "react-icons/fa";
-import { FaGithub as GithubIcon } from "react-icons/fa";
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
+import { FaPhone as PhoneIcon, FaUniversity as UniversityIcon } from "react-icons/fa";
+import {
+  FaGithub as GithubIcon,
+  FaRegCalendarAlt as CalendarIcon,
+  FaBuilding as BuildingIcon,
+} from "react-icons/fa";
+import { IoBookSharp as BooksIcons} from "react-icons/io5"
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
 const Body = () => {
   return (
     <div className={classes.container}>
@@ -16,14 +21,14 @@ const Body = () => {
       </div>
       <div>
         <h3 className={classes.tituloSeccion}>Contact and networks</h3>
-        <p>
+        <p className={classes.zoomingText}>
           <EmailIcon className={classes.icons} /> nicolasbellanich@gmail.com
         </p>
-        <p>
+        <p className={classes.zoomingText}>
           <PhoneIcon className={classes.icons} />
           +54 351-2080462
         </p>
-        <p>
+        <p className={classes.zoomingText}>
           <LinkedinIcon className={classes.icons} />{" "}
           <a
             target="_blank"
@@ -33,7 +38,7 @@ const Body = () => {
             My profile
           </a>
         </p>
-        <p>
+        <p className={classes.zoomingText}>
           <GithubIcon className={classes.icons} />
           <a
             target="_blank"
@@ -47,16 +52,108 @@ const Body = () => {
       <div>
         <h3 className={classes.tituloSeccion}>Job experience</h3>
         <Accordion>
-            <AccordionSummary><p>» Full stack developer</p></AccordionSummary>
-            <AccordionDetails><p>first</p></AccordionDetails>
+          <AccordionSummary>
+            <p>» Full stack developer </p>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p>
+              <div>
+                <CalendarIcon className={classes.icons}/>November 2020 - Present
+              </div>
+              <div>
+                <BuildingIcon className={classes.icons}/> Bitlogic.io
+              </div>
+              <div>
+                <ChatIcon className={classes.icons} />
+                Focus on front end development I´m working with React. Also I
+                help with Java using Springboot. I´m working with figma making
+                some mockups of what we programm after with the team
+              </div>
+            </p>
+          </AccordionDetails>
         </Accordion>
         <Accordion>
-            <AccordionSummary><p>2</p></AccordionSummary>
-            <AccordionDetails><p>second</p></AccordionDetails>
+          <AccordionSummary>
+            <p>» Full stack developer </p>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p>
+              <div>
+                <CalendarIcon className={classes.icons}/>September 2020 - October 2020
+              </div>
+              <div>
+                <BuildingIcon className={classes.icons}/> WeDevelop
+              </div>
+              <div>
+                <ChatIcon className={classes.icons} />
+                I worked as full stack developer for an MVP project. I tryed to implement some
+                mockups at frontend. Also we worked with a mail newsletter at the backend. We also made some 
+                interviews to know better our cliend and worked with lean UX to improve our work time.
+              </div>
+            </p>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary>
+            <p>» Full stack trainee program </p>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p>
+              <div>
+                <CalendarIcon className={classes.icons}/>May 2020 - August 2020
+              </div>
+              <div>
+                <BuildingIcon className={classes.icons}/> WeDevelop
+              </div>
+              <div>
+                <ChatIcon className={classes.icons} />
+                I was selected to be part of this full-stack begginers programm. We worked
+                with the newest technologies such as GraphQL , Apollo-Server & Apollo-Client,
+                React, Node, PostgreSQL and much more. Also we made a lot of research and 
+                had learnigns about UX/UI. Also we met some authentication methods like JWT.
+                It was a very complete experience. After that I was selected to stay on the 
+                company.
+              </div>
+            </p>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary>
+            <p>» Python scrapping  </p>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p>
+              <div>
+                <CalendarIcon className={classes.icons}/>January 2020 - March 2020
+              </div>
+              <div>
+                <BuildingIcon className={classes.icons}/> Freelance
+              </div>
+              <div>
+                <ChatIcon className={classes.icons} />
+                  A friend called me to work side by him in this project for a local
+                  shop in my city. He had some things made by that time but I helped him
+                  for finishing the work. He also teached git-github things. And we helped
+                  the shop to kwon data about the house rental prices on the city. We used Pandas 
+                  and worked with .CSV files to present the data with Excels graphs.
+              </div>
+            </p>
+          </AccordionDetails>
         </Accordion>
       </div>
       <div>
         <h3 className={classes.tituloSeccion}>Education & courses</h3>
+        <p>
+          <div>
+            <UniversityIcon  className={classes.icons}/> I finished my engineer degree on february 2021
+          </div>
+          <div>
+            <UniversityIcon  className={classes.icons}/> I studied English since I was a child. But I got my Cambridge certification on 2015
+          </div>
+          <div>
+            <BooksIcons  className={classes.icons}/> Actually I´m studing by myself thigs related to data science. Such as Tableau or Machine Learning with Python.
+          </div>
+        </p>
       </div>
     </div>
   );
--- src/texts/English/Landing.js
+++ src/texts/English/Landing.js
@@ -1,4 +1,4 @@
 const about_me =
-  "I am a systems engineer in search for an opportunity to challenge me so I can add the maximum of my value. I consider myself a person who can bring joy and respect to working groups. What's more I always like to empathize and Learn from others.";
+  "I am a software engineer searching for an opportunity to challenge me so I can give my best effort. I consider myself a person who can bring joy and respect to working groups. What's more I always like to empathize and learn from others.";
 
 export { about_me };
